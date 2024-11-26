90f2501221bfef2355c95f269dd5d0f0dc671781
Alexandre CAROL <alexandre.carol@etu.univ-nantes.fr>
2024-11-26 11:57:55 +0100

    Code Structuration

---
+++ src/components/FormationList.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Formation } from '@/types'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  name: 'FormationList',
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+
+    const formations = store.state.formations
+
+    const goToFormation = (formation: Formation) => {
+      router.push(`/formations/${formation.id}`)
+    }
+
+    return {
+      formations,
+      goToFormation
+    }
+  }
+})
+
+</script>
+
+<template>
+  <main>
+    <h1>Formations</h1>
+    <ul>
+      <li v-for="formation in formations" :key="formation.id" @click="goToFormation(formation)">
+        {{ formation.name }}
+      </li>
+    </ul>
+  </main>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="logo-container">
+      <RouterLink to="/">
+        <!-- <img :src="beatboxIMG" alt="Beatbox logo" class="logo" /> -->
+        <span class="brand-name">FINDAJOB</span>
+      </RouterLink>
+    </div>
+    <div class="nav-links">
+      <RouterLink to="/" class="nav-link">Home</RouterLink>
+      <RouterLink to="/signup" class="nav-link">SIGN UP</RouterLink>
+      <RouterLink to="/login" class="nav-link">LOGIN</RouterLink>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: var(--color-background-soft);
+}
+
+.logo-container {
+  display: flex;
+  align-items: center;
+}
+
+.logo-container a {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+}
+
+.logo {
+  width: 32px;
+  height: 32px;
+  margin-right: 1rem;
+}
+
+.brand-name {
+  font-size: 2rem;
+  font-weight: 700;
+  letter-spacing: 0.1rem;
+  color: black;
+}
+
+.nav-links {
+  display: flex;
+  gap: 2rem;
+}
+
+.nav-link {
+  color: var(--color-text);
+  text-decoration: none;
+  font-weight: 500;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  transition: background-color 0.3s;
+}
+
+.nav-link:hover {
+  background-color: var(--color-background-mute);
+}
+
+.nav-link.router-link-active {
+  color: blue;
+  background-color: lightcyan;
+}
+
+@media (max-width: 768px) {
+  .navbar {
+    flex-direction: column;
+    padding: 1rem;
+    gap: 1rem;
+  }
+
+  .nav-links {
+    width: 100%;
+    justify-content: center;
+  }
+}
+</style>
