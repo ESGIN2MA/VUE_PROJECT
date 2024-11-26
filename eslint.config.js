cd4d7be26e2c4288f3615bed6f9fd8355c97a8a2
Lahgolz <lahgolz@proton.me>
2024-11-26 15:05:28 +0100

    chore: setup tools

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,190 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslint from '@eslint/js';
+import pluginVitest from '@vitest/eslint-plugin';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import importX from 'eslint-plugin-import-x';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import sonarjs from 'eslint-plugin-sonarjs';
+import unicorn from 'eslint-plugin-unicorn';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
 
 export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
+	{
+		name: 'app/files-to-lint',
+		files: ['**/*.{ts,mts,tsx,vue}'],
+	},
 
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
+	{
+		name: 'app/files-to-ignore',
+		ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/*.d.ts'],
+	},
 
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
-  skipFormatting,
-]
+	eslint.configs.recommended,
+	sonarjs.configs.recommended,
+	unicorn.configs['flat/recommended'],
+	prettierRecommended,
+	...pluginVue.configs['flat/essential'],
+	...vueTsEslintConfig({ extends: ['strictTypeChecked'] }),
+
+	{
+		...pluginVitest.configs.recommended,
+		files: ['src/**/__tests__/*'],
+	},
+	skipFormatting,
+	{
+		plugins: {
+			'import-x': importX,
+		},
+
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+		rules: {
+			'consistent-return': 'off',
+			'class-methods-use-this': 'off',
+			'object-shorthand': [
+				'error',
+				'always',
+				{
+					avoidQuotes: true,
+				},
+			],
+			'no-plusplus': 'off',
+			'no-underscore-dangle': 'off',
+			'no-restricted-syntax': [
+				'error',
+				{
+					selector: 'ForInStatement',
+					message:
+						'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+				},
+				{
+					selector: 'LabeledStatement',
+					message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+				},
+				{
+					selector: 'WithStatement',
+					message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+				},
+			],
+			'no-spaced-func': 'off',
+			'no-irregular-whitespace': 'error',
+			'no-multiple-empty-lines': [
+				'error',
+				{
+					max: 1,
+				},
+			],
+			'one-var': ['error', 'never'],
+			'no-cond-assign': ['error', 'except-parens'],
+			'comma-dangle': 0,
+			eqeqeq: [
+				'error',
+				'always',
+				{
+					null: 'ignore',
+				},
+			],
+			'new-parens': ['error', 'always'],
+			'no-caller': 'error',
+			'no-constant-condition': 'error',
+			'no-control-regex': 'error',
+			'no-debugger': 'error',
+			'no-duplicate-case': 'error',
+			'no-eval': 'error',
+			'no-ex-assign': 'error',
+			'no-extra-boolean-cast': 'error',
+			'no-fallthrough': 'error',
+			'no-inner-declarations': 'error',
+			'no-unused-labels': 'error',
+			'no-proto': 'error',
+			'no-redeclare': 'error',
+			'no-regex-spaces': 'error',
+			'no-self-compare': 'error',
+			'no-sparse-arrays': 'error',
+			'no-negated-in-lhs': 'error',
+			'no-new-wrappers': 'error',
+			'no-self-assign': 'error',
+			'no-this-before-super': 'error',
+			'no-with': 'error',
+			'rest-spread-spacing': ['error', 'never'],
+			'no-trailing-spaces': [
+				'error',
+				{
+					ignoreComments: true,
+				},
+			],
+			'no-undef-init': 'error',
+			'no-unsafe-finally': 'error',
+			'padded-blocks': ['error', 'never'],
+			'space-in-parens': ['error', 'never'],
+			'use-isnan': 'error',
+			'valid-typeof': [
+				'error',
+				{
+					requireStringLiterals: true,
+				},
+			],
+			curly: ['error', 'all'],
+			'handle-callback-err': ['error', '^error$'],
+			'no-array-constructor': 'error',
+			'no-unreachable': 'error',
+			'no-multi-spaces': 'error',
+			'no-unneeded-ternary': 'error',
+			'no-param-reassign': [
+				'error',
+				{
+					props: false,
+				},
+			],
+			'no-continue': 'off',
+
+			'sonarjs/no-duplicate-string': 'off',
+			'sonarjs/no-identical-functions': 'off',
+			'sonarjs/no-inverted-boolean-check': 'error',
+			'sonarjs/cognitive-complexity': 'off',
+
+			'sort-imports': 'off',
+
+			'import-x/no-unresolved': 'off',
+			'import-x/prefer-default-export': 'off',
+			'import-x/no-extraneous-dependencies': 'off',
+			'import-x/extensions': 'off',
+			'import-x/no-named-as-default': 'off',
+			'import-x/no-named-as-default-member': 'off',
+
+			'unicorn/prevent-abbreviations': [
+				'warn',
+				{
+					checkFilenames: false,
+
+					replacements: {
+						env: false,
+						props: false,
+						ref: false,
+					},
+				},
+			],
+			'unicorn/no-useless-undefined': [
+				'error',
+				{
+					checkArguments: false,
+				},
+			],
+			'unicorn/consistent-function-scoping': [
+				'error',
+				{
+					checkArrowFunctions: false,
+				},
+			],
+			'unicorn/no-unused-properties': 'error',
+			'unicorn/no-anonymous-default-export': 'off',
+			'unicorn/filename-case': 'off',
+			'unicorn/no-null': 'off',
+		},
+	},
+];
